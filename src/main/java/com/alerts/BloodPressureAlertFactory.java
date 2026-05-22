6cb606e3fb050838fbc2c20df76638d7c160f741
toma haawk <tomahaawk@tomas-MacBook-Pro.local>
2026-05-22 17:30:29 +0200

    part 4 done i guess but some issue with git push

--- src/main/java/com/alerts/BloodPressureAlertFactory.java
+++ src/main/java/com/alerts/BloodPressureAlertFactory.java
@@ -3,6 +3,12 @@ package com.alerts;
 public class BloodPressureAlertFactory extends AlertFactory {
     @Override
     public Alert createAlert(String patientId, String condition, long timestamp) {
-        return new BloodPressureAlert(patientId, condition, timestamp);
+        Alert alert = new BloodPressureAlert(patientId, condition, timestamp);
+
+        if (condition.contains("Critical") || condition.contains("Hypotensive")) {
+            return new PriorityAlertDecorator(alert, "HIGH");
+        }
+
+        return alert;
     }
 }
